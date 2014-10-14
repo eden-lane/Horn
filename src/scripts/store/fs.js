c6686d880e858dabc6a2f6f2a81c0e9de7ef6b72
eden lane <edencore@gmail.com>
2014-10-14 22:41:50 +0400

    refactored some modules

---
+++ src/scripts/store/fs.js
@@ -0,0 +1,73 @@
+/**
+ * Module for working with local file system
+ *
+ */
+
+;(function (angular) {
+  'use strict';
+
+  function Fs ($q) {
+
+    var settings = {
+      type: 'openWritableFile',
+      accepts: [
+        {
+          extensions: ['md']
+        },
+        {
+          extensions: ['txt']
+        }
+      ]
+    },
+        fs = chrome.fileSystem;
+
+
+    /**
+     * Read file content from file entry
+     *
+     * @param {FileEntry}
+     * @return {Promise<String>} - text content of fileEntry
+     */
+    function readFileEntry (fileEntry) {
+      var defer = $q.defer();
+      fileEntry.file(function (file) {
+        var reader = new FileReader();
+        reader.onloadend = function (e) {
+          defer.resolve(e.target.result);
+        };
+
+        reader.readAsText(file);
+      });
+      return defer.promise;
+    }
+
+    /**
+     * Start a dialog for opening file from
+     * local file system
+     */
+    function open () {
+      var defer = $q.defer();
+      fs.chooseEntry(settings, function (fileEntry) {
+         readFileEntry(fileEntry).then(function (text) {
+           var data = {
+             text: text,
+             name: fileEntry.name
+           }
+
+           defer.resolve(data);
+         });
+      });
+
+      return defer.promise;
+    }
+
+    return {
+      open: open
+    }
+  }
+
+  angular
+    .module('Horn')
+    .factory('Fs', Fs);
+
+})(angular);
