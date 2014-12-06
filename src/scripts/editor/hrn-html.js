953970f395a4049d8f3c658653774499f6311639
eden lane <edencore@gmail.com>
2014-12-06 17:41:54 +0300

    added html mode

--- src/scripts/editor/hrn-html.js
+++ src/scripts/editor/hrn-html.js
@@ -8,24 +8,66 @@
   function hrnHtml () {
     return {
       restrict: 'E',
-      require: ['^editor'],
+      require: '^editor',
+      scope: {
+        text: '='
+      },
       templateUrl: 'scripts/editor/hrn-html.html',
       link: link
     }
     
-    function link (scope, element, attrs, ctrls) {
+    function link (scope, element, attrs, editorCtrl) {
       var cm, doc,
       options = {
-        mode: 'gfm',
+        mode: 'htmlmixed',
         theme: 'kirin',
-        tabSize: 4,
+        tabSize: 2,
         lineWrapping: true,
         readOnly: true
       };
 
-
+      init();
       
+      function init() {
+        var textarea = element.find('textarea')[0];
+        cm = CodeMirror.fromTextArea(textarea, options);
+        cm.setSize('100%', '95%');
+      };
+      
+      scope.$watch(isHtmlMode, setVisibility);
+      
+      scope.$watch('withTemplate', setContent);
+      
+      /**
+       * Check if current mode is HTML
+       */
+      function isHtmlMode () {
+        return editorCtrl.isMode('html');
+      }
+      
+      /**
+       * Shows and hides directive
+       */
+      function setVisibility (isVisible) {
+        if (isVisible) {
+          element.css('display', 'block');
+          setContent();
+          cm.refresh();
+        } else {
+          element.css('display', 'none');
+        }
+      }
 
+      /**
+       * Change content of CodeMirror
+       */
+      function setContent () {
+        var content;
+        content = scope.text;
+          
+        cm.setValue(content);
+      }
+      
     }
   }
   
